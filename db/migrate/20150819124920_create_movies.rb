34cef6c3e411526331ebf0d9cf42deed36e6f450
EmreYavuz216 <emre.yavuz169@gmail.com>
2015-08-19 15:52:44 +0300

    First init and created movies migration

---
+++ db/migrate/20150819124920_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :description
+      t.string :keywords
+      t.text :storyline
+      t.text :techspecs
+
+      t.timestamps null: false
+    end
+  end
+end
